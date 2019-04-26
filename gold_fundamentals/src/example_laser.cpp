43300b91deafa73c0066a20dfeb700c57d6e29f2
Robert Gericke <49953838+robertgericke@users.noreply.github.com>
2019-04-26 12:13:48 +0200

    add whole package dir

---
+++ gold_fundamentals/src/example_laser.cpp
@@ -0,0 +1,19 @@
+#include "ros/ros.h"
+#include <cstdlib>
+#include "sensor_msgs/LaserScan.h"
+
+
+void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
+{
+  ROS_INFO("%i", msg->ranges.size());
+  ROS_INFO("%f", msg->ranges[msg->ranges.size()/2]);
+}
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "example");
+  ros::NodeHandle n;
+  ros::Subscriber sub = n.subscribe("scan_filtered", 1, laserCallback);
+  ros::spin();
+  return 0;
+}
