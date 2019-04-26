3f402c9d1e2b71f2bd9b0e63c9d36710348c7ed2
Robert Gericke <49953838+robertgericke@users.noreply.github.com>
2019-04-26 20:04:21 +0200

    added perfect turn, changed square encoders

--- gold_fundamentals/src/perfect_turn_no_sensor.cpp
+++ gold_fundamentals/src/perfect_turn_no_sensor.cpp
@@ -1,4 +1,73 @@
-//
-// Created by ros on 26.04.19.
-//
+#include "ros/ros.h"
+#include <cmath>
+#include <csignal>
+#include <cstdlib>
+#include "create_fundamentals/DiffDrive.h"
 
+const double ROBOT_WHEEL_RADIUS = 0.032;
+const double ROBOT_TRACK = 0.258;
+const double ROBOT_ANGULAR_SPEED = 2 * M_PI; //max: 15.625
+
+ros::ServiceClient diffDrive;
+
+void brake() {
+    ROS_INFO("braking: diffDrive 0 0");
+    create_fundamentals::DiffDrive srv;
+    srv.request.left = 0;
+    srv.request.right = 0;
+    diffDrive.call(srv);
+}
+
+
+inline double angularToLinear(double angular) {
+    return angular * ROBOT_WHEEL_RADIUS;
+}
+
+
+inline double distanceToTime(double distance, double speed) {
+    return std::abs(distance / angularToLinear(speed));
+}
+
+
+int sgn(double val) {
+    if (val > 0) { return 1; }
+    if (val < 0) { return -1; }
+    return 0;
+}
+
+
+// angle in radians
+void turn(double angle) {
+    double distance = angle * ROBOT_TRACK / 2;
+    double time = distanceToTime(distance, ROBOT_ANGULAR_SPEED);
+    ROS_INFO("turning: diffDrive -10 10 %lf %lf", angle, time);
+    create_fundamentals::DiffDrive srv;
+    srv.request.left = -ROBOT_ANGULAR_SPEED * sgn(angle);
+    srv.request.right = ROBOT_ANGULAR_SPEED * sgn(angle);
+    diffDrive.call(srv);
+
+    ros::Duration(time).sleep();
+    brake();
+}
+
+
+void mySigintHandler(int sig) {
+    ROS_INFO("exiting..");
+    brake();
+
+    ros::shutdown();
+}
+
+
+int main(int argc, char **argv) {
+    signal(SIGINT, mySigintHandler);
+    ros::init(argc, argv, "perfect_turn_no_sensor", ros::init_options::NoSigintHandler);
+    ros::NodeHandle n;
+    diffDrive = n.serviceClient<create_fundamentals::DiffDrive>("diff_drive");
+
+    turn(10 * M_PI);
+
+    mySigintHandler(0);
+
+    return 0;
+}
