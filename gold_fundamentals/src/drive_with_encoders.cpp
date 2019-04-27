66cc4b116d626f7437b6dbe9d77060848bbfa350
Robert Gericke <robert.a.gericke@gmail.com>
2019-04-27 03:56:05 +0200

    added new ideas

---
+++ gold_fundamentals/src/drive_with_encoders.cpp
@@ -0,0 +1,65 @@
+#include "ros/ros.h"
+#include <cmath>
+#include <csignal>
+#include <cstdlib>
+#include "create_fundamentals/DiffDrive.h"
+#include "create_fundamentals/SensorPacket.h"
+#include "Robot.cpp"
+#include "PID.cpp"
+#include "tools.h"
+
+Robot robot;
+
+void sensorCallback(const create_fundamentals::SensorPacket::ConstPtr &msg) {
+    ROS_INFO("left encoder: %lf, right encoder: %lf", msg->encoderLeft, msg->encoderRight);
+    robot.encoderLeft = msg->encoderLeft;
+    robot.encoderRight = msg->encoderRight;
+}
+
+
+void mySigintHandler(int sig) {
+    ROS_INFO("exiting.. sig:%d", sig);
+    robot.diffDrive(0.0, 0.0);
+
+    ros::shutdown();
+}
+
+
+void drive(double distance) {
+    const int RATE = 100;
+
+    ros::spinOnce();
+
+    PID pid = PID(1.0 / RATE, Robot::MAX_SPEED, -Robot::MAX_SPEED, 1.0, 0.0, 0.5);
+
+    double setpoint = robot.encoderLeft + distance * Robot::ENCODER_STEPS_PER_REVOLUTION / (M_PI * 2.0 * Robot::WHEEL_RADIUS);
+    double pv = robot.encoderLeft;
+    double prev_encoder = robot.encoderLeft;
+    double out = 1.0;
+
+    ros::Rate loop_rate(RATE);
+    while (ros::ok() && abs(setpoint - pv) < 0.05) {
+        ros::spinOnce();
+
+        double diff = abs(robot.encoderLeft - prev_encoder) * sgn(out);
+        pv += diff;
+
+        out = pid.calculate(setpoint, pv);
+        robot.diffDrive(out, out);
+        loop_rate.sleep();
+    }
+}
+
+
+int main(int argc, char **argv) {
+    signal(SIGINT, mySigintHandler);
+    ros::init(argc, argv, "square_with_encoders", ros::init_options::NoSigintHandler);
+    ros::NodeHandle n;
+    ros::Subscriber sub = n.subscribe("sensor_packet", 1, sensorCallback);
+    ros::ServiceClient diffDrive = n.serviceClient<create_fundamentals::DiffDrive>("diff_drive");
+    signal(SIGINT, mySigintHandler);
+    robot = Robot(diffDrive);
+    ros::spin();
+
+    return 0;
+}
---
+++ gold_fundamentals/src/tools.cpp
@@ -0,0 +1,10 @@
+//
+// Created by ros on 27.04.19.
+//
+
+#include "tools.h"
+
+
+int sgn(double val) {
+    return (0.0 < val) - (val < 0.0);
+}
