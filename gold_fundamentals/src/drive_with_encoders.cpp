da0a809f90cb0e2286f22c121a93aa09885c78c2
Robert Gericke <49953838+robertgericke@users.noreply.github.com>
2019-04-27 13:35:26 +0200

    added sensor data

--- gold_fundamentals/src/drive_with_encoders.cpp
+++ gold_fundamentals/src/drive_with_encoders.cpp
@@ -12,10 +12,8 @@ Robot robot;
 
 void sensorCallback(const create_fundamentals::SensorPacket::ConstPtr &msg) {
     ROS_INFO("left encoder: %lf, right encoder: %lf", msg->encoderLeft, msg->encoderRight);
-    robot.encoderLeft = msg->encoderLeft;
-    robot.encoderRight = msg->encoderRight;
+    robot.sensorData = msg;
 }
-
 
 void mySigintHandler(int sig) {
     ROS_INFO("exiting.. sig:%d", sig);
@@ -28,17 +26,20 @@ void mySigintHandler(int sig) {
 void drive(double distance) {
     const int RATE = 100;
 
-    ros::spinOnce();
+    while (robot.sensorData == NULL){
+        ros::spinOnce();
+        ros::Duration(0.1).sleep();
+    }
 
-    PID pid = PID(1.0 / RATE, Robot::MAX_SPEED, -Robot::MAX_SPEED, 1.0, 0.0, 0.5);
+    PID pid = PID(1.0 / RATE, Robot::MAX_SPEED, -Robot::MAX_SPEED, 1.0, 0.0, 0.0);
 
-    double setpoint = robot.encoderLeft + distance * Robot::ENCODER_STEPS_PER_REVOLUTION / (M_PI * 2.0 * Robot::WHEEL_RADIUS);
-    double pv = robot.encoderLeft;
-    double prev_encoder = robot.encoderLeft;
+    double setpoint = robot.sensorData->encoderLeft + distance * Robot::ENCODER_STEPS_PER_REVOLUTION / (M_PI * 2.0 * Robot::WHEEL_RADIUS);
+    double pv = robot.sensorData->encoderLeft;
+    double prev_encoder = robot.sensorData->encoderLeft;
     double out = 1.0;
 
     ros::Rate loop_rate(RATE);
-    while (ros::ok() && abs(setpoint - pv) < 0.05) {
+    while (ros::ok() && abs(setpoint - pv) > 0.05) {
         ros::spinOnce();
 
         double diff = abs(robot.encoderLeft - prev_encoder) * sgn(out);
@@ -59,7 +60,8 @@ int main(int argc, char **argv) {
     ros::ServiceClient diffDrive = n.serviceClient<create_fundamentals::DiffDrive>("diff_drive");
     signal(SIGINT, mySigintHandler);
     robot = Robot(diffDrive);
-    ros::spin();
+
+    drive(1.0);
 
     return 0;
 }
