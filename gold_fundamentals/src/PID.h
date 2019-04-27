8e3055542c09755e1cea28640e312afa17b9f800
Robert Gericke <49953838+robertgericke@users.noreply.github.com>
2019-04-27 17:04:32 +0200

    added header for PID and Robot

---
+++ gold_fundamentals/src/PID.h
@@ -0,0 +1,23 @@
+#ifndef SRC_PID_H
+#define SRC_PID_H
+
+class PID {
+public:
+    PID(double dt, double max, double min, double Kp, double Ki, double Kd);
+
+    ~PID();
+
+    double calculate(double setpoint, double pv);
+    double _dt;
+private:
+
+    double _max;
+    double _min;
+    double _Kp;
+    double _Ki;
+    double _Kd;
+    double _pre_error;
+    double _integral;
+};
+
+#endif //SRC_PID_H
