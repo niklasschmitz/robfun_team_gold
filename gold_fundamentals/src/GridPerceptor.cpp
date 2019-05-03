6c6f030b8071f07ab937fed3ecf4680c11e32604
niklasschmitz <niklas.schmitz@outlook.com>
2019-05-03 20:29:00 +0200

    add debug code for lin_reg in laserCallback

--- gold_fundamentals/src/GridPerceptor.cpp
+++ gold_fundamentals/src/GridPerceptor.cpp
@@ -14,10 +14,23 @@ void GridPerceptor::laserCallback(const sensor_msgs::LaserScan::ConstPtr &msg) {
     //ROS_INFO("min deg %f", msg->angle_min);
     //ROS_INFO("min deg %f", msg->angle_increment);
     //ROS_INFO("%f", msg->ranges[msg->ranges.size() / 2]);
+
+    // fill angles vector
+    std::vector<double> theta;
+    theta.push_back(msg->angle_min);
+    for (int i = 1; i < msg->ranges.size(); ++i) {
+        theta.push_back(theta[i-1] + msg->angle_increment);
+    }
+
+    std::vector<std::vector<double>> xy = convertPolarToCartesian(theta, msg->ranges);
+    std::vector<double> coeff = linear_regression(xy[0], xy[1]);
+    ROS_INFO("alpha %lf, beta %lf", coeff[0], coeff[1]);
 }
 
 
 std::vector<std::vector<double>> GridPerceptor::convertPolarToCartesian(std::vector<double> theta, std::vector<double> r) {
+    // TODO handle nan
+
     std::vector<double> x;
     std::vector<double> y;
 
@@ -34,6 +47,8 @@ std::vector<std::vector<double>> GridPerceptor::convertPolarToCartesian(std::vec
 
 
 std::vector<double> GridPerceptor::linear_regression(std::vector<double> x, std::vector<double> y) {
+    // TODO handle nan
+
     // assume (x, y)'s in cartesian coordinates.
     // regress y given x. model: y = alpha + beta*x
 
