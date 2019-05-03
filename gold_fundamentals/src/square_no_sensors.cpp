f05f726c3fde935ea76efb1d14495e19f1d68859
Robert Gericke <49953838+robertgericke@users.noreply.github.com>
2019-05-03 20:54:38 +0200

    fix gridperceptor

--- gold_fundamentals/src/square_no_sensors.cpp
+++ gold_fundamentals/src/square_no_sensors.cpp
@@ -3,22 +3,75 @@
 #include <csignal>
 #include <cstdlib>
 #include "create_fundamentals/DiffDrive.h"
-#include "tools.h"
-#include "Robot.h"
-#include "GridPerceptor.h"
 
 const double ROBOT_WHEEL_RADIUS = 0.032;
 const double ROBOT_TRACK = 0.258;
 const double ROBOT_ANGULAR_SPEED = 5.0; //max: 15.625
 
-Robot* robot;
+ros::ServiceClient diffDrive;
+
+
+void brake() {
+    ROS_INFO("braking: diffDrive 0 0");
+    create_fundamentals::DiffDrive srv;
+    srv.request.left = 0.0f;
+    srv.request.right = 0.0f;
+    diffDrive.call(srv);
+}
+
+
+inline double angularToLinear(double angular) {
+    return angular * ROBOT_WHEEL_RADIUS;
+}
+
+
+inline double distanceToTime(double distance, double speed) {
+    return std::abs(distance / angularToLinear(speed));
+}
+
+
+int sgn(double val) {
+    if (val > 0) { return 1; }
+    if (val < 0) { return -1; }
+    return 0;
+}
+
+
+// angle in radians
+void turn(double angle) {
+    brake();
+
+    double distance = angle * ROBOT_TRACK / 2;
+    double time = distanceToTime(distance, ROBOT_ANGULAR_SPEED);
+    ROS_INFO("turning: diffDrive %lf %lf angle:%lf time:%lf", ROBOT_ANGULAR_SPEED, ROBOT_ANGULAR_SPEED, angle, time);
+    create_fundamentals::DiffDrive srv;
+    srv.request.left = -ROBOT_ANGULAR_SPEED * sgn(angle);
+    srv.request.right = ROBOT_ANGULAR_SPEED * sgn(angle);
+    diffDrive.call(srv);
+
+    ros::Duration(time).sleep();
+    brake();
+}
+
+
+void drive(double distance) {
+    double time = distanceToTime(distance, ROBOT_ANGULAR_SPEED);
+    ROS_INFO("driving: diffDrive %lf %lf", ROBOT_ANGULAR_SPEED, ROBOT_ANGULAR_SPEED);
+    create_fundamentals::DiffDrive srv;
+    srv.request.left = ROBOT_ANGULAR_SPEED;
+    srv.request.right = ROBOT_ANGULAR_SPEED;
+    diffDrive.call(srv);
+
+    ros::Duration(time).sleep();
+    brake();
+}
+
 
 void mySigintHandler(int sig) {
     ROS_INFO("exiting.. sig:%d", sig);
-    robot->brake();
+    brake();
 
     ros::shutdown();
-    delete(robot);
 }
 
 
@@ -27,9 +80,9 @@ void mySigintHandler(int sig) {
 void driveSquare(double side_length, int iterations) {
     // 4 turns make 1 square
     for (int i = 0; i < iterations * 4; ++i) {
-        robot->drive(side_length);
+        drive(side_length);
         ros::Duration(0.5).sleep();
-        robot->turn(M_PI_2);
+        turn(M_PI_2);
         ros::Duration(0.5).sleep();
     }
 }
@@ -38,14 +91,16 @@ void driveSquare(double side_length, int iterations) {
 int main(int argc, char **argv) {
     signal(SIGINT, mySigintHandler);
     ros::init(argc, argv, "square_no_sensors", ros::init_options::NoSigintHandler);
-    robot = new Robot();
+    ros::NodeHandle n;
+    diffDrive = n.serviceClient<create_fundamentals::DiffDrive>("diff_drive");
     signal(SIGINT, mySigintHandler);
+
     if (ros::ok()) {
         // drive 5 consecutive squares
         driveSquare(1., 5);
     }
 
     mySigintHandler(0);
-    delete(robot);
+
     return 0;
 }
