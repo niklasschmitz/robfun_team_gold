f57963f7a0ec7706a12b38f843a2d46e3c0b0038
Fynn <fynn-boyber@web.de>
2019-05-06 00:32:57 +0200

    Implements RANSAC, refactoring

--- gold_fundamentals/src/GridPerceptor.h
+++ gold_fundamentals/src/GridPerceptor.h
@@ -7,20 +7,32 @@
 #include <cmath>
 #include <vector>
 
+typedef struct {
+    double alpha;
+    double beta;
+} T_LINE;
+
+typedef struct {
+    double x;
+    double y;
+} T_CARTESIAN_COORD;
+
 class GridPerceptor {
 public:
     GridPerceptor();
 
     ~GridPerceptor();
 
-
 private:
     void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg);
 
     ros::Subscriber sub_laser;
 
-    std::vector<std::vector<double> > convertPolarToCartesian(std::vector<double> theta, std::vector<double> r);
-    std::vector<double> linear_regression(std::vector<double> x, std::vector<double> y);
+    T_CARTESIAN_COORD convertPolarToCartesian(double theta, double r);
+    T_LINE linear_regression(std::vector<T_CARTESIAN_COORD> coordinates);
+    std::vector<T_LINE> ransac(std::vector<T_CARTESIAN_COORD> coordinates);
+    double distBetweenLineAndPoint(T_LINE line, T_CARTESIAN_COORD point);
+    bool testLineSimilarity(std::vector<T_LINE> lines, T_LINE line);
 };
 
 #endif //SRC_GP_H
