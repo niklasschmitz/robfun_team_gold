af08deadf998bbfc8542cc6645f2ace4db4d8c78
Robert Gericke <robert.a.gericke@gmail.com>
2019-05-07 00:00:59 +0200

    add smooth plan executer

---
+++ gold_fundamentals/src/smooth_executer.cpp
@@ -0,0 +1,64 @@
+#include "ros/ros.h"
+#include <csignal>
+#include <cmath>
+#include "gold_fundamentals/ExecutePlan.h"
+#include "create_fundamentals/DiffDrive.h"
+#include "Robot.h"
+#include "GridPerceptor.h"
+
+
+double const MAZE_SIDE_LENGTH = 0.8;
+
+Robot* robot;
+
+void turn(int direction) {
+    int amount = direction - robot->direction;
+
+    if (amount == 3)
+        amount = -1;
+    if (amount == -3)
+        amount = 1;
+
+    robot->turn(amount * M_PI_2);
+    robot->direction = direction;
+}
+
+bool execute(gold_fundamentals::ExecutePlan::Request &req, gold_fundamentals::ExecutePlan::Response &res) {
+
+    for (int i = 0; i < req.plan.size(); i++) {
+        ROS_INFO("%d", req.plan[i]);
+        turn(req.plan[i]);
+        robot->drive(MAZE_SIDE_LENGTH);
+    }
+
+    res.success = true;
+
+    return true;
+}
+
+void mySigintHandler(int sig) {
+    ROS_INFO("exiting.. sig:%d", sig);
+    robot->diffDrive(0.0, 0.0);
+
+    delete(robot);
+
+    ros::shutdown();
+}
+
+int main(int argc, char **argv) {
+    signal(SIGINT, mySigintHandler);
+    ros::init(argc, argv, "smooth_executer", ros::init_options::NoSigintHandler);
+    ros::NodeHandle n;
+    ros::ServiceServer service = n.advertiseService("execute_plan", execute);
+    signal(SIGINT, mySigintHandler);
+    robot = new Robot();
+    robot->direction = gold_fundamentals::ExecutePlanRequest::UP;
+    robot->turn(M_PI * 2); //simulate align
+
+    ROS_INFO("Ready to execute.");
+    ros::spin();
+
+    delete(robot);
+
+    return 0;
+}
