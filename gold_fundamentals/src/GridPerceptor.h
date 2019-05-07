06fed21b17a194df11dcff1d708033f6f48ea4b4
niklasschmitz <niklas.schmitz@outlook.com>
2019-05-07 15:13:35 +0200

    refactor T_POINT2D struct to add operator overloading for =, ==, +, -, *

--- gold_fundamentals/src/GridPerceptor.h
+++ gold_fundamentals/src/GridPerceptor.h
@@ -8,10 +8,41 @@
 #include <vector>
 
 
-typedef struct {
+struct T_POINT2D {
     double x;
     double y;
-} T_POINT2D;
+
+    // constructor
+    T_POINT2D(double x = 0, double y = 0)
+            : x(x), y(y) {}
+
+    // assignment
+    T_POINT2D &operator=(const T_POINT2D &other) {
+        x = other.x;
+        y = other.y;
+        return *this;
+    }
+
+    // equality
+    bool operator==(const T_POINT2D& other) {
+        return (x == other.x && y == other.y);
+    }
+
+    // addition
+    T_POINT2D operator+(const T_POINT2D &other) const {
+        return T_POINT2D(other.x + x, other.y + y);
+    }
+
+    // subtraction
+    T_POINT2D operator-(const T_POINT2D &other) const {
+        return T_POINT2D(other.x - x, other.y - y);
+    }
+
+    // dot product
+    double operator*(const T_POINT2D &other) const {
+        return other.x * x + other.y * y;
+    }
+};
 
 typedef struct {
     T_POINT2D x0; // support vector
@@ -25,14 +56,18 @@ public:
     ~GridPerceptor();
 
 private:
-    void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg);
+    void laserCallback(const sensor_msgs::LaserScan::ConstPtr &msg);
 
     ros::Subscriber sub_laser;
 
     T_POINT2D convertPolarToCartesian(double theta, double r);
+
     T_LINE constructLineParameterForm(T_POINT2D x1, T_POINT2D x2);
+
     std::vector<T_LINE> ransac(std::vector<T_POINT2D> coordinates);
+
     double distBetweenLineAndPoint(T_LINE line, T_POINT2D point);
+
     bool testLineSimilarity(std::vector<T_LINE> lines, T_LINE line);
 };
 
