8e010427da76b2351f5680713e796447dcf8ae9a
Robert Gericke <49953838+robertgericke@users.noreply.github.com>
2019-05-07 13:13:15 +0200

    change position calculation

--- gold_fundamentals/src/smooth_executer.cpp
+++ gold_fundamentals/src/smooth_executer.cpp
@@ -53,10 +53,11 @@ int main(int argc, char **argv) {
     signal(SIGINT, mySigintHandler);
     robot = new Robot();
     robot->direction = gold_fundamentals::ExecutePlanRequest::UP;
-    robot->turn(M_PI * 2); //simulate align
+    robot->drive(1.0);
+    //robot->turn(M_PI * 2); //simulate align
 
     ROS_INFO("Ready to execute.");
-    ros::spin();
+    //ros::spin();
 
     delete(robot);
 
