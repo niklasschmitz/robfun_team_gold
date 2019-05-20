c4d678dec95a4936978c98c733f094b60eb665b5
niklasschmitz <niklas.schmitz@outlook.com>
2019-05-20 02:07:40 +0200

    add DiscreteLocalizer WIP

---
+++ gold_fundamentals/src/utils/DiscreteLocalizer.cpp
@@ -0,0 +1,12 @@
+#include "DiscreteLocalizer.h"
+
+void DiscreteLocalizer::DiscreteLocalizer() {
+
+}
+
+void DiscreteLocalizer::estimateConfiguration(RobotConfiguration action, maze::Cell observation) {
+    std::vector new_candidates;
+    for (int i = 0; i < this->candidates.size(); ++i) {
+
+    }
+}
