d8da82e8678f2598ea924a4475a71d068d986405
niklasschmitz <niklasschmitz@github.com>
2019-06-04 10:39:01 +0200

    replace endless loop while(true) by while(ros::ok())

--- gold_fundamentals/src/localization_discrete.cpp
+++ gold_fundamentals/src/localization_discrete.cpp
@@ -122,7 +122,7 @@ int main(int argc, char **argv) {
     gold_fundamentals::Pose pose = localization_demo();
 
     // publish the position
-    while (true) {
+    while (ros::ok()) {
         robot->pose_pub.publish(pose);
         ros::Duration(0.5).sleep();
     }
