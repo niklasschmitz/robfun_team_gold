6ecede042271dd934e56171bcd0d05a948cec3f9
niklasschmitz <niklasschmitz@github.com>
2019-06-10 17:34:34 +0200

    add planning demo sketch

--- gold_fundamentals/src/planning.cpp
+++ gold_fundamentals/src/planning.cpp
@@ -2,6 +2,8 @@
 #include <cmath>
 #include <csignal>
 #include <cstdlib>
+#include <DiscreteLocalizer.h>
+#include <PathPlanner.h>
 #include "utils/Robot.h"
 #include "utils/tools.h"
 #include "utils/geometry.h"
@@ -11,6 +13,28 @@ Robot* robot;
 
 bool execute(gold_fundamentals::MoveToPosition::Request &req, gold_fundamentals::MoveToPosition::Response &res) {
     //TODO: drive robot to (x,y)
+
+    DiscreteLocalizer *localizer = new DiscreteLocalizer();
+
+    // wait until map is received
+    while (!localizer->received_map) {
+        ros::Duration(0.5).sleep();
+        ros::spinOnce();
+    }
+
+    Maze* maze = (localizer->maze);
+    PathPlanner planner;
+
+    // TODO: replace start with robot pose
+    Cell* start = maze->getCell(0,0);
+    Cell* goal = maze->getCell(req.row, req.column);
+
+    // calculate path
+    planner.populateBreadthFirstSearch(*maze, goal);
+    std::vector<int> plan = planner.extractPlan(start, goal);
+
+    // follow path
+    robot->executePlan(plan);
 
     res.success = true;
     return true;
@@ -34,6 +58,7 @@ int main(int argc, char **argv) {
     robot = new Robot();
     signal(SIGINT, mySigintHandler);
     robot->localize();
+
     ros::spin();
     delete(robot);
     return 0;
