949767171da61c791682f2a4ac596603a9921e75
lFatality <fynn-boyer@web.de>
2019-06-14 21:09:01 +0200

    BETA VERSION! (Implement code to drive to next closest goal)

--- gold_fundamentals/src/challenge.cpp
+++ gold_fundamentals/src/challenge.cpp
@@ -41,12 +41,46 @@ void pickupCallback(const gold_fundamentals::Goal::ConstPtr &msg) {
     }
 }
 
-int driveTo(T_VECTOR2D point) {
+//int driveTo(T_VECTOR2D point) {
+//    DiscreteLocalizer localizer;
+//
+//    // wait until map is received
+//    while (!localizer.received_map) {
+//        ros::Duration(0.5).sleep();
+//        ros::spinOnce();
+//    }
+//
+//    Maze* maze = (localizer.maze);
+//    PathPlanner planner;
+//
+//    Cell* start = maze->getCell(robot->getCellxy());
+//    Cell* goal = maze->getCell(point.x, point.y);
+//
+//    // calculate path
+//    planner.populateBreadthFirstSearch(*maze, goal);
+//    std::vector<int> plan = planner.extractPlan(start, goal);
+//
+//    // follow path
+//    robot->executePlan(plan);
+//
+//    bool arrived = robot->getCellxy() == point;
+//    return arrived;
+//}
+
+int drivePlan(std::vector<int> plan, T_VECTOR2D goal_point) {
+    // follow path
+    robot->executePlan(plan);
+
+    bool arrived = robot->getCellxy() == goal_point;
+    return arrived;
+}
+
+std::vector<int> getPlanToPoint(T_VECTOR2D point) {
     DiscreteLocalizer localizer;
 
     // wait until map is received
     while (!localizer.received_map) {
-        ros::Duration(0.5).sleep();
+        ros::Duration(0.1).sleep();
         ros::spinOnce();
     }
 
@@ -60,11 +94,70 @@ int driveTo(T_VECTOR2D point) {
     planner.populateBreadthFirstSearch(*maze, goal);
     std::vector<int> plan = planner.extractPlan(start, goal);
 
-    // follow path
-    robot->executePlan(plan);
+    return plan;
+}
 
-    bool arrived = robot->getCellxy() == point;
-    return arrived;
+struct BestPlan {
+    std::vector<int> bestPlan;
+    int index;
+
+    BestPlan(std::vector<int> bestPlan, int index) : bestPlan(bestPlan), index(index) {}
+};
+
+BestPlan getBestPlan(std::vector<std::vector<int > > possible_plans) {
+    std::vector<int> best_plan;
+    int best_plan_length = 1000;
+    int best_plan_index = 500;
+    for(int plan_idx=0; plan_idx < possible_plans.size(); plan_idx++) {
+        int plan_length = possible_plans[plan_idx].size();
+        if(plan_length < best_plan_length) {
+            best_plan = possible_plans[plan_idx];
+            best_plan_length = plan_length;
+            best_plan_index = plan_idx;
+        }
+    }
+
+    return BestPlan(best_plan, best_plan_index);
+}
+
+void traverse_positions(std::vector<T_VECTOR2D>* positions) {
+    while(positions->size() > 0) {
+        //collect plans to all remaining positions
+        std::vector<std::vector<int> > possible_plans;
+        for(T_VECTOR2D g: *positions) {
+            possible_plans.push_back(getPlanToPoint(g));
+        }
+
+        BestPlan bestPlan = getBestPlan(possible_plans);
+
+        // remove the position we will drive to now and execute the plan
+        if(!drivePlan(bestPlan.bestPlan, positions->at(bestPlan.index))) {
+           continue;
+        } else {
+            robot->digForGold();
+            positions->erase(positions->begin() + bestPlan.index);
+        }
+    }
+}
+
+void driveToShortestGoal(std::vector<T_VECTOR2D>* positions) {
+    bool arrived = false;
+    while(!arrived) {
+        //collect plans to all remaining positions
+        std::vector<std::vector<int> > possible_plans;
+        for(T_VECTOR2D g: *positions) {
+            possible_plans.push_back(getPlanToPoint(g));
+        }
+
+        BestPlan bestPlan = getBestPlan(possible_plans);
+
+        // remove the position we will drive to now and execute the plan
+        if(!drivePlan(bestPlan.bestPlan, positions->at(bestPlan.index))) {
+            continue;
+        } else {
+            arrived = true;
+        }
+    }
 }
 
 int main(int argc, char **argv) {
@@ -82,33 +175,21 @@ int main(int argc, char **argv) {
 
     while (!gold){
         ROS_INFO("waiting for gold");
-        ros::Duration(0.5).sleep();
+        ros::Duration(0.1).sleep();
         ros::spinOnce();
     }
 
     while (!pickup){
         ROS_INFO("waiting for pickup");
-        ros::Duration(0.5).sleep();
+        ros::Duration(0.1).sleep();
         ros::spinOnce();
     }
 
-    //pickup gold and drive to Helipad
-    for (T_VECTOR2D g: *gold) {
-        while(!driveTo(g)) {
-            ROS_INFO("Retry Gold (%lf/%lf)", g.x, g.y);
-        }
-        robot->digForGold();
-        ROS_INFO("gold count %d", robot->gold_count);
-    }
+    traverse_positions(gold);
+    driveToShortestGoal(pickup);
 
-    for (T_VECTOR2D p: *pickup) {
-        while(!driveTo(p)) {
-
-        }
-        //TODO pickup
-        robot->playSong(1);
-        mySigintHandler(0);
-    }
+    robot->playSong(1);
+//    mySigintHandler(0);
 
     ROS_INFO("We got %d gold.", robot->gold_count);
 
